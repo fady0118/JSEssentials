8b853bd32d86bccb9bb727e3033f5254eb054e9d
fady0118 <fady0118@gmail.com>
2024-11-12 22:36:32 +0200

    added new features

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,11 +10,19 @@ function calculateArea(){
     let length = parseFloat(document.getElementById("length").value);
     let width = parseFloat(document.getElementById("width").value);
     let area = length * width;
+
     let result = document.getElementById("result");
-    if(length<0 || width<0){
+    if(isNaN(area)){
+        result.innerHTML="Missing Input!";
+        document.getElementById("length").classList.add("error");
+        document.getElementById("width").classList.add("error");
+    }
+    else if(length<0 || width<0){
         result.innerHTML="Invalid Input!"
     }
     else{
+        document.getElementById("length").classList.remove("error");
+        document.getElementById("width").classList.remove("error");
         result.innerHTML=`The area of the rectangle is: ${area}`;
     }
 }
